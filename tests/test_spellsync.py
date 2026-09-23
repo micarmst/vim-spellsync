7c42e2b0e211b7baa4e436e4afce16afb0f472e8
micarmst <124457306+emlowz@users.noreply.github.com>
2026-09-23 17:16:46 +0100

    Use native runtime path escaping in Windows fixtures

--- tests/test_spellsync.py
+++ tests/test_spellsync.py
@@ -333,7 +333,10 @@ class SpellSyncTests(unittest.TestCase):
                     call assert_equal(original_isfname, &isfname)
                     call assert_notmatch('SpellSync:', execute('messages'))
                 """.replace('DIRECTORY', directory), before=r"""
-                    let &runtimepath .= ',' . escape(g:test_root . '/DIRECTORY', '[],{}')
+                    " Windows treats brackets/braces literally; backslashes are separators.
+                    let escapes = has('win32') ? ',' : '[],{}'
+                    let &runtimepath .= ',' . escape(g:test_root . '/DIRECTORY', escapes)
+                    call assert_false(empty(globpath(&runtimepath, 'spell', 1, 1)))
                     call TestSpelling()
                     call assert_equal(['spellsyncword', 'bad'], spellbadword('spellsyncword'))
                 """.replace('DIRECTORY', directory))
